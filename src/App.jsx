5ae675885ed0fe2b5bf66bfe84a3de7034ab7eea
mahmoud01294 <mahmoud.hassan01116@gmail.com>
2025-11-29 01:54:52 +0200

    first version of react lms app

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Home from "./Components/Home/Home";
+import Courses from "./Components/Courses/Courses";
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Accounts from "./Components/Accounts/Accounts";
+import Layout from "./Components/Layout/Layout";
+import { createBrowserRouter } from "react-router-dom";
+
+let x = createBrowserRouter([
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      { path: "home", element: <Home /> },
+      { path: "courses", element: <Courses /> },
+      { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> },
+      { path: "accounts", element: <Accounts /> },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={x}></RouterProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='fixed-bottom bg-dark text-center p-4' >
+        <h3 className='text-white'>footer</h3>
+    </div>
+  )
+}
+
+export default Footer
